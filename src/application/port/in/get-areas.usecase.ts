678b086ea20974145305ed2ab0d8da996fd9b8d6
berkcansavur <berkcansavur@gmail.com>
2025-04-27 11:38:45 +0300

    typo fix

--- src/application/port/in/get-areas.usecase.ts
+++ src/application/port/in/get-areas.usecase.ts
@@ -1,6 +1,5 @@
-import { CachedAreas } from '@/shared/types';
+import { CachedAreas } from '../../service/area-cache.service';
 import { AreaDomain } from '../../../domain/area/area.domain';
-
 export interface GetAreasUseCase {
   findAll(): Promise<AreaDomain[]>;
   findNearestAreas(longitude: number, latitude: number, limit?: number): Promise<CachedAreas | null>;
